0b184fe575b76369b5a006e9203bd5d003c9e07f
Agnes-99 <Agnesmabusela8@gmail.com>
2025-04-12 13:32:38 +0200

    Added locations to the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,23 +3,54 @@ import '../styles/Hero.css';
 
 const Hero = () => {
   return (
-    <section className="hero" id="hero"        
-    style={{
-      backgroundImage: "url('/assets/slideimages/slideImage_3.jpg')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      height: '85vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      overflow: 'hidden',
-      backgroundColor: '#14213d',
-    }} >
+    <section
+      className="hero"
+      id="hero"
+      style={{
+        backgroundImage: "url('/assets/slideimages/slideImage_3.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '85vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        backgroundColor: '#14213d',
+        position: 'relative',
+      }}
+    >
       <div className="hero-content">
         <h1>Transform Your Home with Solar Solutions</h1>
-        <p>Choose from our exclusive inverter packages that cater to your specific energy needs and budget.</p>
-        <a href="#packages-section" className="cta-button-packages">Explore Our Packages</a>
+        <div className="hero-locations">
+          <h2>Now Operating In:</h2>
+          <ul>
+            <li>Cape Town</li>
+            <li>Stellenbosch</li>
+            <li>Paarl</li>
+            <li>Worcester</li>
+            <li>East London</li>
+            <li>Gqeberha</li>
+            <li>Bloemfontein</li>
+            <li>Welkom</li>
+            <li>Johannesburg</li>
+            <li>Pretoria</li>
+            <li>Mbombela</li>
+            <li>eMalahleni</li>
+            <li>Kimberly</li>
+            <li>Kuruman</li>
+            <li>Durban</li>
+            <li>Newcastle</li>
+            <li>Pietermaritzburg</li>
+            <li>Polokwane</li>
+            <li>Mokopane</li>
+            <li>Tzaneen</li>
+            <li>Jane Furse</li>
+            <li>Lebowakgomo</li>
+            <li>Groblersdal</li>
+      
+          </ul>
+        </div>
       </div>
     </section>
   );
